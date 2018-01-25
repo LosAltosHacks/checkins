cf0f38352310115a8192ec8d6900f4ec108348bf
Dan Appel <dan.appel00@gmail.com>
2018-01-24 16:29:16 -0800

    Initial commit with xcode project & qr code reader

---
+++ Checkins/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Checkins
+//
+//  Created by Dan Appel on 1/24/18.
+//  Copyright © 2018 Los Altos Hacks. All rights reserved.
+//
+
+import UIKit
+import QRCodeReader
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
